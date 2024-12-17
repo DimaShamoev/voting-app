6bd9c96bce3d864478ca8995e93b5f28cf6ca2ab
Dima Shamoev <dimashamoev5@gmail.com>
2024-12-17 19:08:33 +0400

    cleaned and refreshed

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,27 +1,9 @@
-import { SignIn, SignInButton,SignedIn,SignedOut,UserButton,useUser } from "@clerk/clerk-react";
 import React from "react";
 
 const App: React.FunctionComponent = () => {
-    const { user } = useUser();
-
-    if (!user) {
-        return (
-            <div>
-                <SignedOut>
-                    
-                </SignedOut>
-            </div>
-        );
-    }
-
     return (
         <div className="App">
-            <SignedIn>
-                Signed In
-                <UserButton />
-                <p>{user?.username}</p>
-                <p>Your User ID: {user?.id}</p>
-            </SignedIn>
+            
         </div>
     );
 };
